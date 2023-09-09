455f92164808f13ce9c0089a7bf659d2b5aaf303
Byron Salvador <byron.salvador@epn.edu.ec>
2023-09-08 22:55:01 -0500

    Hola mundo con vue y con un ligero style

---
+++ hola-mundo-vue/src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <HolaMundo message="Hola Mundo" />
+    <HolaMundo message2="Leonardo Salvador" />
+    
+  </div>
+  
+</template>
+
+<script setup>
+import HolaMundo from './components/HolaMundo.vue'
+</script>
---
+++ hola-mundo-vue/src/components/HolaMundo.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h1>{{ message }}</h1>
+        <h4>{{ message2 }}</h4>
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  message: String,
+  message2: String,
+})
+</script>
+
+<style>
+#app{
+    text-align: center;
+    color: blueviolet;
+}
+</style>
